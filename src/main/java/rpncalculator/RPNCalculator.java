235f1235a3a797395b798219421c0453c52afd3a
chevassu <chevassu.florian@gmail.com>
2019-01-11 18:05:00 +0100

    Version avec 2 operandes

--- src/main/java/rpncalculator/RPNCalculator.java
+++ src/main/java/rpncalculator/RPNCalculator.java
@@ -1,4 +1,45 @@
 package rpncalculator;
 
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
 public class RPNCalculator {
+    public static int calculate(String input) {
+        if (input.contains(" ")) {
+            String[] operandes = input.split(" ");
+            int operande1 = Integer.parseInt(operandes[0]);
+            int operande2 = Integer.parseInt(operandes[1]);
+
+            Operation operation = Operation.of(operandes[2]);
+            return operation.calculate(operande1, operande2);
+        }
+        return Integer.parseInt(input);
+    }
+
+    private enum Operation {
+        ADD("+", (a, b) -> a + b),
+        SUB("-", (a, b) -> a - b),
+        DIV("/", (a, b) -> a / b),
+        MUL("*", (a, b) -> a * b);
+
+        private BiFunction<Integer, Integer, Integer> operation;
+
+        Operation(String operator, BiFunction<Integer, Integer, Integer> operation) {
+            this.operation = operation;
+            this.operator = operator;
+        }
+
+        private String operator;
+
+        public static Operation of(String operator) {
+            return Arrays.stream(values())
+                    .filter(operation -> operation.operator.equals(operator))
+                    .findFirst()
+                    .get();
+        }
+
+        public int calculate(int operande1, int operande2) {
+            return this.operation.apply(operande1, operande2);
+        }
+    }
 }
--- src/test/java/rpncalculator/RPNCalculatorTest.java
+++ src/test/java/rpncalculator/RPNCalculatorTest.java
@@ -4,5 +4,89 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class RPNCalculatorTest {
-    
+    @Test
+    public void should_return_1_when_input_is_1(){
+        // Given
+        String input = "1";
+        int expectedResult = 1;
+        // When
+        int result = RPNCalculator.calculate(input);
+        // Then
+        Assertions.assertThat(result).isEqualTo(expectedResult);
+    }
+    @Test
+    public void should_return_6_when_input_is_6(){
+        // Given
+        String input = "6";
+        int expectedResult = 6;
+        // When
+        int result = RPNCalculator.calculate(input);
+        // Then
+        Assertions.assertThat(result).isEqualTo(expectedResult);
+    }
+    @Test
+    public void should_return_61_when_input_is_61(){
+        // Given
+        String input = "61";
+        int expectedResult = 61;
+        // When
+        int result = RPNCalculator.calculate(input);
+        // Then
+        Assertions.assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void should_return_6_when_input_is_3_3_plus(){
+        // Given
+        String input = "3 3 +";
+        int expectedResult = 6;
+        // When
+        int result = RPNCalculator.calculate(input);
+        // Then
+        Assertions.assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void should_return_22_when_input_is_20_2_plus(){
+        // Given
+        String input = "20 2 +";
+        int expectedResult = 22;
+        // When
+        int result = RPNCalculator.calculate(input);
+        // Then
+        Assertions.assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void should_return_9_when_input_is_3_3_mul(){
+        // Given
+        String input = "3 3 *";
+        int expectedResult = 9;
+        // When
+        int result = RPNCalculator.calculate(input);
+        // Then
+        Assertions.assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void should_return_4_when_input_is_8_4_minus(){
+        // Given
+        String input = "8 4 -";
+        int expectedResult = 4;
+        // When
+        int result = RPNCalculator.calculate(input);
+        // Then
+        Assertions.assertThat(result).isEqualTo(expectedResult);
+    }
+
+    @Test
+    public void should_return_4_when_input_is_8_2_div(){
+        // Given
+        String input = "8 2 /";
+        int expectedResult = 4;
+        // When
+        int result = RPNCalculator.calculate(input);
+        // Then
+        Assertions.assertThat(result).isEqualTo(expectedResult);
+    }
 }
